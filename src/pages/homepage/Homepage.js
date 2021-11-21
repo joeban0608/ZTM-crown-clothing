ac6698f2caa80e327cbf09d38c79f36234930e7e
joeban0608 <80736596+joeban0608@users.noreply.github.com>
2021-11-21 20:54:01 +0800

    create crown-clothing-homepage and adding styles

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Homepage.scss"
+import DirectoryMenu from "../../components/DirectoryMenu/DirectoryMenu"
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <DirectoryMenu />
+    </div>
+  )
+}
+
+export default HomePage;
